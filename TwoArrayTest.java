0dea537b52a4101bec6fdcf5caa41e8e6c12ba14
HyeonMin Choi <58718743+hmhmchm@users.noreply.github.com>
2020-01-06 21:09:25 +0900

    Create TwoArrayTest.java

---
+++ TwoArrayTest.java
@@ -0,0 +1,24 @@
+public class TwoArrayTest {
+
+	public static void main(String[] args) {
+		int arr[][] = new int [3][];
+		arr[0] = new int[1];
+		arr[1] = new int[2];
+		arr[2] = new int[3];
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = i + j;
+			}
+		}
+		
+		for(int[] n : arr) {
+			for(int k : n) {
+				System.out.print(k + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
