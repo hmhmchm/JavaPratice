5651fcfcb82d3e4a16c58e9fdd21afaeb89d316c
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-02-08 17:29:34 +0900

    Create CircleDemo1.java

---
+++ CircleDemo1.java
@@ -0,0 +1,19 @@
+class Circle{
+	double radius;
+	static int numOfCircles = 0;
+	int numCircles = 0;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+		numOfCircles++;
+		numCircles++;
+	}
+}
+public class CircleDemo1 {
+
+	public static void main(String[] args) {
+		//수정 
+
+	}
+
+}
