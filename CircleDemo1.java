230bd1ea13236e1057a68619f027161d640758e9
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-02-09 20:50:08 +0900

    Update CircleDemo1.java

--- CircleDemo1.java
+++ CircleDemo1.java
@@ -10,10 +10,17 @@ class Circle{
 	}
 }
 public class CircleDemo1 {
-
 	public static void main(String[] args) {
-		//수정 
-
+		Circle myCircle = new Circle(10.0);
+		Circle yourCircle = new Circle(5.0);
+		
+		//print();  print메서드를 불러오려면 밑에 정적인 static으로 묶어야한다.
+		System.out.println("원의 개수 : " + Circle.numOfCircles);
+		System.out.println("원의 개수 : " + yourCircle.numCircles);
 	}
-
+	
+	void print() {
+		System.out.println("정적 멤버에 대한 내용입니다.");
+	}
+		
 }
