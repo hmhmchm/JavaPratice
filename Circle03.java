32c8e2dbd764342ae2684e5b84807d9eb9dda90f
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-02-22 22:45:55 +0900

    Create Circle03.java

---
+++ Circle03.java
@@ -0,0 +1,29 @@
+
+public class Circle03 {
+	private void secret() {
+		System.out.println("비밀이다.");
+	}
+	
+	protected void findRadius() {
+		System.out.println("반지름이 10.0");
+	}
+	
+	public void findArea() {
+		System.out.println("넓이는 (파이 * 반지름 *반지름) 이다.");
+	}
+}
+class Ball extends Circle03{
+	private String color;
+	
+	public Ball(String color) {
+		this.color = color;
+	}
+	
+	public void findColor() {
+		System.out.println(color + " 공이다.");
+	}
+	public void findVolume() {
+		System.out.println("부피는 4/3 * (파이*반지름*반지름) 이다.");
+	}
+	
+}
